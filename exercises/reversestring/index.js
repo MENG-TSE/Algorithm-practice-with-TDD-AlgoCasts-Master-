07624b18374cfb0176b80f3b5ffaf019be10e73f
MENG-TSE <jimmylin0179@gmail.com>
2018-08-14 23:24:43 +0800

    string reversed

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,27 +7,38 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-    let reversed = '';
-
-    // for( var i = 0 ; i<str.length; i++){
-
-    // }
-
-    for(let character of str) {
-        reversed = charaters + reversed;
-    }
-
-    return reversed;
+  return  str.split('').reduce((reversed, character) =>  character + reversed, '');
 }
 
 module.exports = reverse;
 
 //法一
+    //function reverse(str){
     // const arr = str.split('');
     // arr.reverse();
     // return arr.join('');
+    //}
+
 //法一(簡寫)
+    //function reverse(str){
     // return str
     //     .split('')
     //     .reverse()
     //     .join('');
+    // }
+
+
+
+    //法二
+    // function reverse(str) {
+    //     let reversed = '';
+                        //------>不建議的方法
+                            //     for( var i = 0 ; i<str.length; i++){
+                            
+                            //     }
+                        //<-------end
+    //     for(let character of str) {
+    //         reversed = charaters + reversed;
+    //     }
+    //     return reversed;
+    // }
