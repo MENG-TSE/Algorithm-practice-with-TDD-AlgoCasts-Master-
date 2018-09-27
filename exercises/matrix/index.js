e0cba82336e5c7dcc104c396975176aed2250b58
MENG-TSE <jimmylin0179@gmail.com>
2018-09-27 15:24:27 +0800

    matrix finish

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,58 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+const results = [];
 
+for(let i = 0; i< n; i++){
+    results.push([]);
+}
+
+let counter = 1;
+let startColumn = 0;
+let endColumn = n - 1;
+while (startColumn <= endColumn && startRow <= endRow){
+    //Top row
+    for(let i = startColumn; i <= endColumn; i++){
+        results[startRow][i] = counter;
+        counter++;
+    }
+    startRow++;
+
+    //Right column
+    for(let i = startRow; i <= endRow; i++ ){
+        results[i][startColumn] = counter;
+        counter++
+    }
+    startColumn--;
+
+    //button row
+    for(let i = endColumn; i >= startColumn; i--){
+        results[endRow][i] = counter;
+        counter++;
+    }
+    endRow--;
+
+    //start column
+    for(let i = endRow; i>= startColumn; i--){
+        results[i][endColumn] = counter;
+        counter++;
+    }
+    startColumn++;
+}
+}
+
+// 提示
+// Create empty array of arrays called 'results'
+// Create a counter variable,starting at 1
+// As long as(start column <= end column) AND (start row <= end row)
+//     Loop from start column to end column
+//         At resultes[start_row][i] assign counter variable
+//         Increment start now
+//     Increment start row
+//     Loop from start row to end row
+//         At results[i][end_column] assign variable
+//         Increment counter
+//     Decrement end row
+//     ...repeat for other two sides
 module.exports = matrix;
